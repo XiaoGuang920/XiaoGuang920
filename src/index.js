924f678d05646037acc8a5180d815c35cbab6c2c
Aaron <aaron.wang@howsense.shop>
2024-07-09 23:51:18 +0800

    mod: change ci/cd actions

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/XiaoGuang920">
             <Header />
             <Routes>
                 <Route index exact path="/" element={<About />}/>
